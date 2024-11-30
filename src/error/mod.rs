6fcc6cd948c009d732dc1b085ce2875ab95b465d
0drng <none>
2024-11-30 03:11:49 +0100

    fmt

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -14,15 +14,25 @@ pub enum PackageManagerError {
     NotInstalled(Labels, Option<Vec<String>>),
     WhichIsNotInstalled(Labels, Option<Vec<String>>),
     InstallFailed(Labels, Option<Vec<String>>),
+    UninstallFailed(Labels, Option<Vec<String>>),
 }
 
 impl From<PackageManagerError> for ApplicationError {
     fn from(value: PackageManagerError) -> Self {
         match value {
-            PackageManagerError::NotInstalled(label, params) => ApplicationError::new(label, params),
-            PackageManagerError::WhichIsNotInstalled(label, params) => ApplicationError::new(label, params),
-            PackageManagerError::InstallFailed(label, params) => ApplicationError::new(label, params),
-        }
+            PackageManagerError::NotInstalled(label, params) => {
+                ApplicationError::new(label, params)
+            },
+            PackageManagerError::WhichIsNotInstalled(label, params) => {
+                ApplicationError::new(label, params)
+            },
+            PackageManagerError::InstallFailed(label, params) => {
+                ApplicationError::new(label, params)
+            },
+            PackageManagerError::UninstallFailed(label, params) => {
+                ApplicationError::new(label, params)
+            }
+         }
     }
 }
 
@@ -34,8 +44,12 @@ pub enum CommandError {
 impl From<CommandError> for PackageManagerError {
     fn from(value: CommandError) -> Self {
         match value {
-            CommandError::UserAbort(labels, params) => PackageManagerError::InstallFailed(labels, params),
-            CommandError::CommandFailed(labels, params) => PackageManagerError::InstallFailed(labels, params),
+            CommandError::UserAbort(labels, params) => {
+                PackageManagerError::InstallFailed(labels, params)
+            }
+            CommandError::CommandFailed(labels, params) => {
+                PackageManagerError::InstallFailed(labels, params)
+            }
         }
     }
 }
